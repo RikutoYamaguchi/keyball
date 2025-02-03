1ff8849b69220a1b464f76e0fe3ce9a7b68e2a51
Rikuto Yamaguchi <yamaguchirkt@gmail.com>
2025-02-03 23:53:01 +0900

    [MOD] add s mins

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/qwerty/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/qwerty/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_universal(
     _______ ,  KC_1    , KC_2      , KC_3    , KC_4    , KC_5       ,                                         KC_6    , KC_7     , KC_8     , KC_9     , KC_0     , _______ ,
     _______ ,  KC_MINS , S(KC_LBRC), S(KC_9) , S(KC_0) , S(KC_RBRC) ,                                         KC_LEFT , KC_DOWN  , KC_UP    , KC_RIGHT , KC_QUOT  , KC_LBRC ,
-    _______ ,  KC_EQL ,  KC_BSLS  , KC_LBRC , KC_RBRC , KC_GRV    ,                                         KC_BSPC   , XXXXXXX  , XXXXXXX  , _______  , _______  , KC_RBRC ,
+    _______ ,  KC_EQL ,  KC_BSLS  , KC_LBRC , KC_RBRC , KC_GRV    ,                                         KC_BSPC   , XXXXXXX  , XXXXXXX  , _______  , S(KC_MINS), KC_RBRC ,
                   _______  , _______ , _______  ,         _______  , _______  ,                         _______  , _______  , _______       , _______  , _______
   ),
 
